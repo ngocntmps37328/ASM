335d27c60631e2f6ab5486c4dd659c5bfda847bf
MinhNgoc <minhngoc9499@gmail.com>
2024-06-06 20:45:57 +0700

    Merge branch 'main' of https://github.com/ngocntmps37328/ASM
    # Conflicts:
    #	js/index.js
    #	wishlist.html

--- js/index.js
+++ js/index.js
@@ -1,135 +1,113 @@
-angular
-  .module("myapp", ["ngRoute"])
-  .run(function ($rootScope, $timeout) {
-    $rootScope.$on("$routeChangeStart", function () {
-      $rootScope.loading = true;
-    });
-    $rootScope.$on("$routeChangeSuccess", function () {
-      $timeout(function () {
-        $rootScope.loading = false;
-      }, 100);
-    });
-    $rootScope.$on("$routeChangeError", function () {
-      $rootScope.loading = false;
-      alert("Lỗi không tải được trang");
-    });
+angular.module('myapp',["ngRoute"])
+.run(function($rootScope,$timeout){
+$rootScope.$on('$routeChangeStart',function(){
+    $rootScope.loading=true;
+});
+$rootScope.$on('$routeChangeSuccess',function(){
+    $timeout(function(){
+    $rootScope.loading=false;},1000);
+});
+$rootScope.$on('$routeChangeError',function(){
+    $rootScope.loading=false;
+    alert('Lỗi không tải được trang');
+});
+})
+.config(function($routeProvider){
+$routeProvider
+.when('/',{
+    templateUrl:'home.html?'+ Math.random(),
+    controller:'homectrl'
+})
+.when('/listproduct',{
+    templateUrl:'listproduct.html?'+ Math.random(),
+    controller:'listproductctrl'
+})
+.when("/wishlist", {
+    templateUrl: "wishlist.html?" + Math.random(),
+    controller: "wishlistAllctrl",
   })
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when("/", {
-        templateUrl: "home.html?" + Math.random(),
-        controller: "homectrl",
-      })
-      .when("/listproduct", {
-        templateUrl: "listproduct.html?" + Math.random(),
-        controller: "listproductctrl",
-      })
-      .when("/cart", {
-        templateUrl: "cart.html?" + Math.random(),
-        controller: "cartctrl",
-      })
-      .when("/wishlist", {
-        templateUrl: "wishlist.html?" + Math.random(),
-        controller: "wishlistAllctrl",
-      })
-      .when("/wishlist/:id", {
-        templateUrl: "wishlist.html?" + Math.random(),
-        controller: "wishlistAddctrl",
-      })
-      .when("/detail/:id", {
-        templateUrl: "detail.html?" + Math.random(),
-        controller: "detailctrl",
-      })
-      .otherwise({
-        template: "<h1>404 - NOT FOUND</h1>",
-      });
+  .when('/cart', {
+    templateUrl: 'cart.html?' + Math.random(),
+    controller: "cartctrl",
   })
+.when('/wishlist/:id',{
+    templateUrl:'wishlist.html?'+ Math.random(),
+    controller:'wishlistctrl'
+})
+.when('/detail/:id',{
+    templateUrl:'detail.html?'+ Math.random(),
+    controller:'detailctrl'
+})
+.otherwise({
+    template:'<h1>404 - NOT FOUND</h1>',
+})
+})
 
-  .controller("myctrl", function ($scope, $http) {
-    $scope.products = [];
-    $http.get("data.json?").then(
-      function (res) {
-        //thành công
+.controller('myctrl',function($scope,$http,$rootScope){
+    $rootScope.products=[]  ;
+    $http.get('data.json?').
+    then(function(res){//thành công
         console.log(res);
-        $scope.products = res.data;
-      },
-      function (res) {
-        //thất bại
-        alert("Lỗi không tải được dữ liệu");
-      }
-    );
-  })
-  .controller("listproductctrl", function ($scope, $http) {
-    $scope.products = [];
-    $http.get("data.json?").then(
-      function (res) {
-        //thành công
-        console.log(res);
-        $scope.products = res.data;
-      },
-      function (res) {
-        //thất bại
-        alert("Lỗi không tải được dữ liệu");
-      }
-    );
-    console.log($scope.products);
-  })
-
-  .service('WishlistService', function() {
-    var wishlist = [];
-  
-    return {
-      getWishlist: function() {
-        return wishlist;
-      },
-      addProduct: function(product) {
-        if (!wishlist.find((item) => item.id == product.id)) {
-          wishlist.push(product);
-        }
-      }
-    };
-  })
-  
-  .controller("wishlistAddctrl", function ($scope, $routeParams, WishlistService) {
-    $scope.id = $routeParams.id;
-    var prdId = $scope.id;
-    var products = $scope.products;
-    var prdGet = products.find((item) => item.id == prdId);
-    if (prdGet) {
-      WishlistService.addProduct(prdGet);
+        $rootScope.products=res.data;
+    },
+    function(res){//thất bại
+       alert('Lỗi không tải được dữ liệu');
+    });
+    
+}) 
+.filter('searchProduct',function(){
+    return function(input,keyword,attr){
+    if(!keyword){
+        return input;
     }
-  })
-  
-  .controller("wishlistAllctrl", function ($scope, WishlistService) {
-    $scope.wishlist = WishlistService.getWishlist();
-    console.log($scope.wishlist);
-  })
+    var output = [];
+    for(var i =0;i<input.length;i++){
+    for(j=0;j<attr.length;j++){
+    if(input[i][attr[j]].toString().toLowerCase().indexOf(keyword.toLowerCase())>=0){
+        output.push(input[i]);
+        break;
+    }}
+    }
+    return output;
+    }})
+    
+.controller('listproductctrl',function($scope,$http,$rootScope){
+    $scope.products = [];
+    $http.get('data.json').then(function(res) {
+        $scope.products = res.data;
+    }, function(res) {
+        alert('Lỗi không tải được dữ liệu');
+    });
+    $scope.wishlist = $rootScope.wishlist;
+    $scope.isInWishlist = function(id) {
+        if ($scope.wishlist) {
+            return $scope.wishlist.find(item => item.id == id);
+        }
+        return false;
+    };
+    $scope.minPrice = 0; 
+    $scope.maxPrice = Infinity; 
+    $scope.filterByPrice = function(min, max) {
+        $scope.minPrice = min;
+        $scope.maxPrice = max;
+        $scope.selectedPriceRange = {min: min, max: max};
+    };
+})    
+.filter('filterPrice',function(){
+    return function(input,min,max){
+        var output=[];
+        for(var i=0;i<input.length;i++){
+            if(input[i].price>=min && input[i].price<=max){
+                output.push(input[i]);
+            }
+        }
+        return output;
+    }
+})
+.controller('detailctrl', function($scope, $routeParams, $rootScope) {
 
-//   .controller("wishlistAddctrl", function ($scope, $routeParams) {
-//     $scope.id = $routeParams.id;
-//     var wishlist = $scope.wishlist || [];
-//     var prdId = $scope.id;
-//     var products = $scope.products;
-//     var prdGet = products.find((item) => item.id == prdId);
-//     console.log(prdGet);
-//     if (prdGet && !wishlist.find((item) => item.id == prdGet.id)){
-//         wishlist.push(prdGet);
-//     }
-//     console.log(wishlist);
-//     $scope.wishlist = wishlist;
-//     $scope.$emit('wishlistGet', wishlist);
-//   })
-
-//   .controller("wishlistAllctrl", function ($scope) {
-//     $scope.$on('wishlistGet', function(event, data) {
-//         $scope.wishlist = data;
-//         console.log(data);
-//     });
-//   })
-
-  .controller("detailctrl", function ($scope, $routeParams) {
     $scope.id = $routeParams.id;
-
+    $rootScope.selectedQuantity = 1;
     var products = $scope.products || [];
 
     var item = products.find((item) => item.id == $scope.id);
@@ -139,10 +117,154 @@ angular
     } else {
       console.error("Product not found");
     }
+    $scope.currentIndex = 0;
+    
+    
+    $scope.showCarousel = function(index) { 
+    
+        $scope.currentIndex = index;
+    };
+    $scope.colorindex = 0;
+    $scope.changeColorname = function(index) { 
+        $scope.colorindex = index;
+        
+    }
+})
+.controller('cartctrl',function(){
+   
+})
+.controller('modalCtrl',function($scope,$rootScope, $routeParams){
+    $scope.id = $routeParams.id;
+    if (!$rootScope.cart) {
+        $rootScope.cart = [];
+    }
+    $scope.cart = $rootScope.cart;
+
+    if (!$rootScope.products) {
+        $rootScope.products = []; // Khởi tạo mảng products nếu chưa có
+    }
+    $scope.products = $rootScope.products;
+
+    // Initialize selected quantity
+    $scope.selectedQuantity = 1;
+
+    // Find the product by ID
+    var item = $scope.products.find(item => item.id == $scope.id);
+    if (item) {
+        $scope.cartItem = item;
+        $scope.cartItem.quantity = $scope.selectedQuantity;
+        // Check if the product is already in the cart
+        if (!$rootScope.cart.find(cartItem => cartItem.id == item.id)) {
+            $rootScope.cart.push($scope.cartItem);
+        }
+    } else {
+        console.error('Product not found');
+    }
+     // Function to decrement quantity
+     $scope.decrementQuantity = function(cartItem) {
+        if (cartItem.quantity > 1) {
+            cartItem.quantity--;
+        }
+    };
+
+    // Function to increment quantity
+    $scope.incrementQuantity = function(cartItem) {
+        cartItem.quantity++;
+    };
+    // Function to update quantity
+    $scope.updateQuantity = function(cartItem) {
+        cartItem.quantity = $scope.selectedQuantity;
+    };
+    // Function to delete an item from the cart
+    $scope.delete = function(index, event) {
+        if (event) {
+            event.preventDefault(); // Prevent the default action
+            event.stopPropagation(); // Stop the event from bubbling up
+        }
+
+        $rootScope.cart.splice(index, 1);
+    };
+
+    $scope.getTotalPrice=function(){
+        var total=0;
+        for(var i=0;i<$scope.cart.length;i++){
+            total+= $scope.cart[i].price*$scope.cart[i].quantity;
+        }
+        return total;
+    };
+    console.log("cart", $scope.cart);
+})
+.controller('homectrl',function(){
+
+})
+.controller('aboutctrl',function(){
+
+})
+
+
+.controller('wishlistctrl', function($scope, $routeParams, $rootScope){
+    if (!$rootScope.wishlist) {
+        $rootScope.wishlist = [];
+    }
+    $scope.wishlist = $rootScope.wishlist;
+    $scope.id = $routeParams.id;
+
+    // Ensure products array exists
+    if (!$scope.products) {
+        $scope.products = [];
+    }
+
+    // Find the product by ID
+    var item = $scope.products.find(item => item.id == $scope.id);
+    if (item) {
+        $scope.sp = item;
+  // Check if the product is already in the wishlist
+        if (!$rootScope.wishlist.find(sp => sp.id == item.id)) {
+            $rootScope.wishlist.push($scope.sp);
+        }
+    } else {
+        console.error('Product not found');
+    }
+
+    // Function to delete an item from the wishlist
+    $scope.delete = function(index, event) {
+        if (event) {
+            event.preventDefault(); // Prevent the default action
+            event.stopPropagation(); // Stop the event from bubbling up
+        }
+
+        $rootScope.wishlist.splice(index, 1);
+        console.log("wishlist", $scope.wishlist);
+
+        if ($rootScope.wishlist.length == 0) {
+            document.querySelector('.container .row').innerHTML = "Không có sản phẩm nào trong danh sách yêu thích";
+        }
+    };
+    console.log("wishlist", $scope.wishlist);
+})
+
+
+.controller("wishlistAllctrl", function ($scope, $rootScope) {
+    $scope.wishlist = $rootScope.wishlist;
+    
+    if ( $scope.wishlist.length == 0) {
+        document.querySelector('.container .row').innerHTML = "Không có sản phẩm nào trong danh sách yêu thích";
+    }
+    $scope.delete = function(index, event) {
+        if (event) {
+            event.preventDefault(); // Prevent the default action
+            event.stopPropagation(); // Stop the event from bubbling up
+        }
+
+        $rootScope.wishlist.splice(index, 1);
+        console.log("wishlist", $scope.wishlist);
+
+        if ($rootScope.wishlist.length == 0) {
+            document.querySelector('.container .row').innerHTML = "Không có sản phẩm nào trong danh sách yêu thích";
+        }
+    };
+    console.log($scope.wishlist);
   })
-
-  .controller("homectrl", function () {})
-
-  .controller("aboutctrl", function () {})
-
-  .controller("contactctrl", function () {});
+.controller('contactctrl',function(){
+    
+});
